35f74dd9b09ecbdb5aed1a48784c55b1a362d381
Erick <erickmorais@lampp-it.com.br>
2023-08-01 14:56:19 -0300

    Test Develop

--- src/base/RacaRPG.java
+++ src/base/RacaRPG.java
@@ -1,19 +1,19 @@
-package workquest;
-
-import workquest.storage.FichaRPG;
-
-public class RacaRPG extends FichaRPG {
-
-	// Aprimoramento de Valores de Raças
-	
-	static int upMountainDwarfRace = strength +2;
-	static int upHillDwarfRace = wisdom + 1;
-	static int upDarkElfRace = charisma + 1;
-	static int upHighElfRace = intelligence + 1;
-	static int upWoodElfRace = wisdom + 1;
-	static int upRockGnomeRace = constitution + 1;
-	static int upGroveGnomeRace = dexterity + 1;
-	static int upLightFootHalflingRace = charisma + 1;
-	static int upStoutHalflingRace = constitution + 1;
-	
-}
+//package workquest;
+//
+//import workquest.storage.FichaRPG;
+//
+//public class RacaRPG extends FichaRPG {
+//
+//	// Aprimoramento de Valores de Raças
+//
+//	static int upMountainDwarfRace = strength +2;
+//	static int upHillDwarfRace = wisdom + 1;
+//	static int upDarkElfRace = charisma + 1;
+//	static int upHighElfRace = intelligence + 1;
+//	static int upWoodElfRace = wisdom + 1;
+//	static int upRockGnomeRace = constitution + 1;
+//	static int upGroveGnomeRace = dexterity + 1;
+//	static int upLightFootHalflingRace = charisma + 1;
+//	static int upStoutHalflingRace = constitution + 1;
+//
+//}
